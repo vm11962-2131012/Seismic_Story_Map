62da2f93c9f6d9939456160915c575da498ddd1c
Victoria Martinez <92124097+vm11962-2131012@users.noreply.github.com>
2024-11-14 14:26:23 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,13 @@ var config = {
     accessToken: 'pk.eyJ1Ijoidm0xMTk2MiIsImEiOiJjbTFqbHFsNWwxMGxxMmlweXdjYm9qZnNoIn0.Ca6Vt9ybed8zD4ZopE358Q',
     showMarkers: true,
     markerColor: '#3FB1CE',
-    //projection: 'equirectangular',
-    //Read more about available projections here
-    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
     theme: 'dark',
-    use3dTerrain: false, //set true for enabling 3D maps.
+    use3dTerrain: true, // map set to 3d 
     auto: false,
     title: 'Seismic Hazards and Recent Earthquake activity',
     subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Victoria and Yi Hong',
+    byline: 'By a Victoria Martinez and Yi Hong',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -57,12 +54,6 @@ var config = {
                 zoom: 8.5,
                 pitch: 60,
                 bearing: -43.2,
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 1, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
