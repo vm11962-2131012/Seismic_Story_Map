d94927f4597db2da806128e1367d5343716157a1
Victoria Martinez <92124097+vm11962-2131012@users.noreply.github.com>
2024-11-14 14:24:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -10,16 +10,16 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
+    title: 'Seismic Hazards and Recent Earthquake activity',
     subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    byline: 'By a Victoria and Yi Hong',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'INTRO CHAPTER',
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
@@ -49,7 +49,7 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'SEISMIC ACTIVITY CHAPTER',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -74,7 +74,7 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'EARTHQUAKE ACTIVITY CHAPTER',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -93,7 +93,7 @@ var config = {
             id: 'fourth-chapter',
             alignment: 'fully',
             hidden: false,
-            title: 'Third Title',
+            title: 'SEISMIC+EARTHQUAKE = HIGH RISK AREAS',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
